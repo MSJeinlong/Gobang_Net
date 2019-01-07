433a1a4344ce75d64b633015f8c767d0b8469cc4
junlong <2869897445@qq.com>
2019-01-07 17:01:04 +0800

    finish finally, completed version

---
+++ mysql/Gobang.sql
@@ -0,0 +1,16 @@
+/*
+ Navicat MySQL Data Transfer
+
+ Source Server         : MySQL
+ Source Server Type    : MySQL
+ Source Server Version : 80013
+ Source Host           : localhost:3306
+ Source Schema         : Gobang
+
+ Target Server Type    : MySQL
+ Target Server Version : 80013
+ File Encoding         : 65001
+
+ Date: 21/12/2018 19:30:04
+*/
+
